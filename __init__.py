d786b285729cfc8b2d87227040dcf45547e2250f
Andrew Hernandez <121366254+ApaxPhoenix@users.noreply.github.com>
2024-05-01 20:41:12 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,1 @@
-from .app import WebPyApp
-from .core import WebPyCore
-from .exceptions import CustomException
-from .utils import some_utility_function
-
-__all__ = '*'
+# File with no content yet...
