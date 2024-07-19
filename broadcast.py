0f0c6075a109e977b1641fe559e4c20b1609ceb6
Andrew Hernandez <121366254+ApaxPhoenix@users.noreply.github.com>
2024-07-19 01:33:23 -0400

    Update broadcast.py

--- broadcast.py
+++ broadcast.py
@@ -1,6 +1,5 @@
 import json
-from urllib.parse import parse_qs
-
+from urllib.parse import parse_qs, urlparse
 
 class Request:
     def __init__(self, handler):
@@ -11,6 +10,8 @@ class Request:
             handler: The HTTP request handler.
         """
         self.handler = handler
+        self._parsed_url = urlparse(self.handler.path)
+        self._query_params = parse_qs(self._parsed_url.query)
 
     @property
     def method(self):
@@ -30,7 +31,17 @@ class Request:
         Returns:
             str: The request path.
         """
-        return self.handler.path
+        return self._parsed_url.path
+    
+    @property
+    def fragment(self):
+        """
+        Get the URL fragment.
+
+        Returns:
+            str: The URL fragment.
+        """
+        return self._parsed_url.fragment
 
     @property
     def headers(self):
@@ -41,6 +52,16 @@ class Request:
             dict: A dictionary containing the request headers.
         """
         return self.handler.headers
+
+    @property
+    def query_params(self):
+        """
+        Get the query parameters.
+
+        Returns:
+            dict: A dictionary of query parameters parsed from the URL.
+        """
+        return self._query_params
 
     def json(self):
         """
@@ -53,8 +74,7 @@ class Request:
         if content_length:
             post_data = self.handler.rfile.read(content_length)
             return json.loads(post_data.decode('utf-8'))
-        else:
-            return {}
+        return {}
 
 
 class Response:
@@ -71,10 +91,13 @@ class Response:
         self.body = b''
 
     def send(self):
-        """Send the response."""
+        """
+        Send the response.
+        """
         self.handler.send_response(self.status_code)
         for key, value in self.headers.items():
             self.handler.send_header(key, value)
+        self.handler.send_header('Content-Length', str(len(self.body)))
         self.handler.end_headers()
         self.handler.wfile.write(self.body)
 
@@ -87,4 +110,25 @@ class Response:
         """
         self.headers['Content-Type'] = 'application/json'
         self.body = json.dumps(data).encode('utf-8')
+        self.send()
+
+    def api(self, data=None):
+        """
+        Send an API response with appropriate status and data based on the HTTP method.
+
+        Args:
+            data (dict, optional): The data to be included in the response.
+        """
+        method = self.handler.command
+        context = {
+            'GET': lambda: (data if data is not None else {}, 200),
+            'POST': lambda: ({"message": "Resource created", "data": data} if data is not None else {"message": "Resource created"}, 201),
+            'PUT': lambda: ({"message": "Resource updated", "data": data} if data is not None else {"message": "Resource updated"}, 200),
+            'DELETE': lambda: ({"message": "Resource deleted"}, 204),
+        }
+
+        response_data, status_code = context.get(method, lambda: ({"error": "Method not allowed"}, 405))()
+        self.status_code = status_code
+        self.headers['Content-Type'] = 'application/json'
+        self.body = json.dumps(response_data).encode('utf-8')
         self.send()
