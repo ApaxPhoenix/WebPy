7e03653e4613e8206aee2a7476d2c815ba603866
Andrew Hernandez <121366254+ApaxPhoenix@users.noreply.github.com>
2024-11-01 09:52:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,12 @@ setup(
     version='0.1.0',
     author='Andrew Hernandez',
     author_email='andromedeyz@hotmail.com',
-    description='A brief description of your library',
+    description='A web framework for building Python applications with ease.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/ApaxPhoenix/WebPy',
     packages=find_packages(),
-    install_requires=[
-        # List your dependencies here, e.g. 'numpy', 'requests',
-    ],
+    install_requires=open('requirements.txt').read().splitlines(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
